18fc2a1e754a7d990b98dae9edab9f85b01b142c
Gabriele224 <gabryricca2001@gmail.com>
2025-12-08 17:58:29 +0100

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -117,239 +117,18 @@ def genera_pdf(df_combined):
     return bytes(pdf.output(dest="S"))
 
 
-
-st.title("Diario del Diabete")
-
-view_diario= st.selectbox("Scegli cosa Visualizzare\n",["UtenteCentro","DiarioPasti","AlimentoConsumato",
-                                                        "DatiCorporei","MediaGlicemia","EmogloGlicata",
-                                                        "TotKcal","TotCho","TotInsulina","Lista Alimenti",
-                                                        "Media DCorporei","HealthSmart"])
-if view_diario:
-    if view_diario=="UtenteCentro":
-
-        db_utente=({
-            "Username": db_utente["username"],
-            "Nome": db_utente["NomeUtente"],
-            "Cognome": db_utente["CognomeUtente"],
-            "Data":db_utente["dataNascita"],
-            "Citta": db_utente["CittaUtente"],
-            "CfUtente":db_utente["CfUtente"],
-            "Centro": db_utente["CentroDiabetologico"]
-        })
-        st.dataframe(db_utente)
-        
-    elif view_diario == "DiarioPasti":
-        db_Pasto=({
-            "Glicemia": db_Pasto["glicemia"],
-            "tipoPasto": db_Pasto["tipoPasto"],
-            "orario": db_Pasto["orario"],
-            "data":db_Pasto["data"],
-            "note": db_Pasto["note"]
-        })
-        st.dataframe(db_Pasto)
-
-    elif view_diario == "AlimentoConsumato":
-        db_alimento=({
-            "Alimento": db_alimento["nomeAlimento"],
-            "TotPeso": db_alimento["totPeso"],
-            "TotCho": db_alimento["totCho"],
-            "TotKcal": db_alimento["totKcal"],
-            "Insulina": db_alimento["insulina"]
-        })
-
-        st.dataframe(db_alimento)
-
-    elif view_diario == "DatiCorporei":
-
-        db_pesoPersonale=({
-            "PesoCorporeo": db_pesoPersonale["pesoPersonale"],
-            "MassaCorporea": db_pesoPersonale["massaCorporea"],
-            "Data": db_pesoPersonale["data"]
-        })
-
-        st.dataframe(db_pesoPersonale)
-    elif view_diario == "MediaGlicemia":
-
-        valori_glicemia= db_Pasto["glicemia"]
-        try:
-            glicemia=np.mean(valori_glicemia)
-            
-            db_valoriGlicemia=({
-                "Media Glicemia": glicemia
-            })
-            st.dataframe(db_valoriGlicemia)
-        except Exception as e:
-            st.error(f"Riprovare. Valori glicemici non presenti nel db\nInserisci nei pasti e riprova\n{e}")
-
-    elif view_diario == "EmogloGlicata":
-
-        valori_glicemia= db_Pasto["glicemia"]
-        try:
-            mediaGlicemia= np.mean(valori_glicemia)
-            emogloglicata= (mediaGlicemia + 47.6) / 27.6
-            
-            db_EmogloGlicata=({
-                "Media Glicemia": mediaGlicemia,
-                "EmoglobiGlicata": emogloglicata
-            })
-            st.dataframe(db_EmogloGlicata)
-        except Exception as e:
-            st.error(f"Riprovare. Valori glicemici non presenti nel db\nInserisci nei pasti e riprova\n{e}")
-
-    elif view_diario == "TotKcal":
-
-        valori_Kcal= db_alimento["totKcal"]
-        try:
-            
-            # Converto sempre tutto in float, scartando ciò che non si può convertire
-            valori_Kcal_float = []
-            for v in valori_Kcal:
-                try:
-                    valori_Kcal_float.append(float(v))
-                except (ValueError, TypeError):
-                    # ignora stringhe vuote, None o cose non numeriche
-                    continue
-            
-            totKcal=float(sum(valori_Kcal_float))
-            
-            db_valoreKcal=({
-                "Totale Kcal": [totKcal]
-            })
-            st.dataframe(db_valoreKcal)
-        except Exception as e:
-            st.error(f"Riprovare. Valori kcal non presenti nel db\nInserisci nei alimenti e riprova\n{e}")
-
-    elif view_diario == "TotCho":
-
-        valori_cho= db_alimento["totCho"]
-        try:
-            
-            # Converto sempre tutto in float, scartando ciò che non si può convertire
-            valori_cho_float = []
-            for v in valori_cho:
-                try:
-                    valori_cho_float.append(float(v))
-                except (ValueError, TypeError):
-                    # ignora stringhe vuote, None o cose non numeriche
-                    continue
-            
-            totcho=float(sum(valori_cho_float))
-            
-            db_valorecho=({
-                "Totale Cho": [totcho]
-            })
-            st.dataframe(db_valorecho)
-        except Exception as e:
-            st.error(f"Riprovare. Valori Cho non presenti nel db\nInserisci nei alimenti e riprova\n{e}")
-
-    elif view_diario == "TotInsulina":
-
-        valori_insulina= db_alimento["insulina"]
-        try:
-            
-            # Converto sempre tutto in float, scartando ciò che non si può convertire
-            valori_insulina_float = []
-            for v in valori_insulina:
-                try:
-                    valori_insulina_float.append(float(v))
-                except (ValueError, TypeError):
-                    # ignora stringhe vuote, None o cose non numeriche
-                    continue
-            
-            totinsulina=float(sum(valori_insulina_float))
-            
-            db_valoreInsulina=({
-                "Totale Insulina": [totinsulina]
-            })
-            st.dataframe(db_valoreInsulina)
-        except Exception as e:
-            st.error(f"Riprovare. Valori Insulina non presenti nel db\nInserisci nei alimenti e riprova\n{e}")
-
-    elif view_diario == "Lista Alimenti":
-        
-        listaAlimenti= db_alimento["nomeAlimento"]
-        if listaAlimenti is not db_alimento:
-            db_alimento=({
-                "Alimento": listaAlimenti,
-                "TotPeso": db_alimento["totPeso"],
-                "TotCho": db_alimento["totCho"],
-                "TotKcal": db_alimento["totKcal"]
-            })
-        st.dataframe(db_alimento)
-
-    elif view_diario == "Media DCorporei":
-
-        valori_pesocorporeo= db_pesoPersonale["pesoPersonale"]
-
-        valori_MC= db_pesoPersonale["massaCorporea"]
-
-        try:
-            pesiCorporei=np.mean(valori_pesocorporeo)
-            
-            massaCorporea=np.mean(valori_MC)
-
-            db_valoriPC=({
-                "Media P.C\nPesoCorporeo": pesiCorporei,
-                "Media M.C\nMassaCorporea":  massaCorporea
-            })
-            st.dataframe(db_valoriPC)
-
-        except Exception as e:
-            st.error(f"Riprovare. Valori del peso e massa corporea non presenti nel db\nInserisci nei pesi e riprova\n{e}")
-    
-    elif view_diario == "HealthSmart":
-
-        valori_health= db_healthsmart["health"]
-
-        valori_oxygen= db_healthsmart["oxygen"]
-
-        try:
-            
-            # Converto sempre tutto in float, scartando ciò che non si può convertire
-            valori_health_float = []
-            valori_oxygen_float=[]
-            for v in valori_health:
-                try:
-                    valori_health_float.append(float(v))
-                except (ValueError, TypeError):
-                    # ignora stringhe vuote, None o cose non numeriche
-                    continue
-
-            tothealth=float(np.mean(valori_health_float))
-
-
-            for v in valori_oxygen:
-                try:
-                    valori_oxygen_float.append(float(v))
-                except (ValueError, TypeError):
-                    # ignora stringhe vuote, None o cose non numeriche
-                    continue
-
-            totoxygen=float(np.mean(valori_oxygen_float))
-
-            db_valorehealth=({
-                "Media Cuore": [tothealth],
-                "Media Ossigeno": [totoxygen]
-            })
-            st.dataframe(db_valorehealth)
-        except Exception as e:
-            st.error(f"Riprovare. Valori cuore e ossigeno non presenti nel db\nInserisci e riprova\n{e}")
-        
-else:
-    
-    st.error("Riprovare.")
-
 #PDF COMPLETO
 st.subheader("PDF")
-date_inizio=st.date_input("Data D'Inizio")
-date_fine=st.date_input("Data Di Fine")
-            
-lista_utenti = db_utente["Username"].tolist()
-username_sel = st.selectbox("Seleziona Username", lista_utenti)
 
 view_pdf= st.selectbox("Visualizza PDF\n",["PDF"])
 if view_pdf:
     if view_pdf=="PDF":
+
+        date_inizio=st.date_input("Data D'Inizio")
+        date_fine=st.date_input("Data Di Fine")
+            
+        lista_utents = db_utente["username"].tolist()
+        username_list = st.selectbox("User", lista_utents)
 
         # Controlliamo che le colonne chiave esistano
         if "id_pasto" not in db_Pasto.columns:
@@ -384,7 +163,7 @@ if view_pdf:
         st.download_button(
             label="📄 Scarica PDF",
             data=pdf_bytes,
-            file_name=f"{username_sel} - {data_inizio_str} e {data_fine_str}.pdf",
+            file_name=f"{username_list} - {data_inizio_str} e {data_fine_str}.pdf",
             mime="application/pdf"
         )
 else:
@@ -434,7 +213,7 @@ elif insert_diario == "DiarioPasto":
                 st.warning("⚠️ Nessun utente registrato.")
                 st.stop()
             
-            lista_utenti = db_utente["Username"].tolist()
+            lista_utenti = db_utente["username"].dropna().astype(str).tolist()
             username_sel = st.selectbox("Seleziona Username", lista_utenti)
 
             # esempio per DiarioPasti
@@ -456,10 +235,10 @@ elif insert_diario == "DiarioPasto":
 elif insert_diario == "Alimento":
     with st.form("form_alimento"):
         nomeAlimento = st.text_input("Alimento")
-        totPeso = st.number_input("TotPeso",min_value=0.0,max_value=1000.0,format="%.2f")
-        totCho = st.number_input("TotCho",min_value=0.0,max_value=1000.0,format="%.2f")
-        totKcal = st.number_input("TotKcal",min_value=0.0,max_value=1000.0,format="%.2f")
-        insulina = st.number_input("Insulina",min_value=0.0,max_value=1000.0,format="%.2f")
+        totPeso = st.number_input("TotPeso",max_value=1000.0,format="%.2f")
+        totCho = st.number_input("TotCho",max_value=1000.0,format="%.2f")
+        totKcal = st.number_input("TotKcal",max_value=1000.0,format="%.2f")
+        insulina = st.number_input("Insulina",max_value=100.0,format="%.2f")
         
 
         try:
@@ -471,11 +250,12 @@ elif insert_diario == "Alimento":
                st.warning("⚠️ Nessun pasto per la data selezionata.")
             
             opzioni_pasto = (
-                db_filtrato["id_pasto"].astype(str)
-                + " - " + db_filtrato["tipoPasto"]
-                + " (" + db_filtrato["data"] + ")"
-                + " (" + db_filtrato["usernameId"] + ")"
+                    db_filtrato["id_pasto"].astype(str)
+                    + " - " + db_filtrato["tipoPasto"].astype(str)
+                    + " (" + db_filtrato["data"].astype(str) + ")"
+                    + " (" + db_filtrato["usernameId"].astype(str) + ")"
             )
+
             scelta = st.selectbox("Scegli il pasto", opzioni_pasto)
             id_pasto_sel = int(scelta.split(" - ")[0])
 
@@ -532,8 +312,8 @@ elif insert_diario == "HealthSmart":
         
         ora = st.text_input("Orario")
         data = st.date_input("Data")
-        health = st.number_input("Cuore")
-        oxygen= st.number_input("Ossigeno")
+        health = st.number_input("Cuore",max_value=1000,format="%.2f")
+        oxygen= st.number_input("Ossigeno",max_value=1000,format="%.2f")
         stress= st.text_input("Stress")
         note = st.text_input("Note")
         
